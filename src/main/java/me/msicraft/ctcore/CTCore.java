0ca9eb37813c2b958175df0f21c5f852b60ec0b9
msicraft <rlarua9689@gmail.com>
2024-05-11 01:53:34 +0900

    update

--- src/main/java/me/msicraft/ctcore/CTCore.java
+++ src/main/java/me/msicraft/ctcore/CTCore.java
@@ -1,6 +1,12 @@
 package me.msicraft.ctcore;
 
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public final class CTCore extends JavaPlugin {
 
@@ -13,10 +19,25 @@ public final class CTCore extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
+        createConfigFiles();
     }
 
     @Override
     public void onDisable() {
+    }
+
+    private void createConfigFiles() {
+        File configf = new File(getDataFolder(), "config.yml");
+        if (!configf.exists()) {
+            configf.getParentFile().mkdirs();
+            saveResource("config.yml", false);
+        }
+        FileConfiguration config = new YamlConfiguration();
+        try {
+            config.load(configf);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 
 }
